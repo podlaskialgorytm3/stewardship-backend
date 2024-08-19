a883a40291736ba5d0de7268903f3cf1b6bdd487
podlaskialgorytm <live.m851@gmail.com>
2024-08-20 00:32:12 +0200

    refactor: remove console.log statements in convertDate function

--- src/utils/task-info.ts
+++ src/utils/task-info.ts
@@ -1,13 +1,7 @@
 const convertDate = (dateString: string) => {
   const date = new Date(dateString);
-
-  console.log("DATA POWAŻNA", dateString);
-
   date.setHours(date.getHours() + 2);
   const newDateString = date.toISOString().slice(0, 19);
-
-  console.log("DATA NOWA POWAŻNA", newDateString);
-
   return newDateString;
 };
 
