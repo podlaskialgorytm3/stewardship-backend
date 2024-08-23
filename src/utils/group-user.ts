8a69cabd06a5fff736afa2f66aaa205e0a126c82
podlaskialgorytm <live.m851@gmail.com>
2024-08-23 21:52:17 +0200

    refactor: change types in groupuser utils

--- src/utils/group-user.ts
+++ src/utils/group-user.ts
@@ -1,15 +1,17 @@
-import UserController from "../controllers/user"
-import GroupUserController from "../controllers/group-user"
+import UserController from "../controllers/user";
+import GroupUserController from "../controllers/group-user";
 
 class GroupUserUtils {
-    private userController = new UserController();
-    private groupUserController = new GroupUserController();
-    public getRole = async (groupId: string, token: string) => {
-        const user = await this.userController.getUserByToken(token);
-        const groupUser = await this.groupUserController.getUser(groupId, user?.id as number) as {role: string};
-        return groupUser.role;
-
-    }
+  private userController = new UserController();
+  private groupUserController = new GroupUserController();
+  public getRole = async (groupId: string, token: string) => {
+    const user = await this.userController.getUserByToken(token);
+    const groupUser = (await this.groupUserController.getUser(
+      groupId,
+      user?.id as string
+    )) as { role: string };
+    return groupUser.role;
+  };
 }
 
 export default GroupUserUtils;
