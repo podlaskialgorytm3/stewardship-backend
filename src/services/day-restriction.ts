698cc59b3df2d24b721f193bac4552bb13d741a7
podlaskialgorytm <live.m851@gmail.com>
2024-09-14 12:39:23 +0200

    feat: create service to adding day restriction to given schedule rule

--- src/services/day-restriction.ts
+++ src/services/day-restriction.ts
@@ -1,6 +1,16 @@
 import { DayRestrictionModal } from "../models/day-restriction";
 
+import { v4 as uuidv4 } from "uuid";
+
+import { DayRestrictionSchema } from "../types/day-restriction";
+
+import GroupUserService from "./group-user";
+
 class DayRestrictionService {
+  groupUserService: GroupUserService;
+  constructor() {
+    this.groupUserService = new GroupUserService();
+  }
   public createTable = async () => {
     DayRestrictionModal.sync({ alter: true })
       .then(() => {
@@ -12,6 +22,56 @@ class DayRestrictionService {
           error
         );
       });
+  };
+  public createDayRestriction = async ({
+    scheduleRuleId,
+    dayOfWeek,
+    maxFollowingDay,
+    groupId,
+    token,
+  }: {
+    scheduleRuleId: string;
+    dayOfWeek: string;
+    maxFollowingDay: number;
+    groupId: string;
+    token: string;
+  }) => {
+    try {
+      const role = await this.groupUserService.getRole({ groupId, token });
+      if (role !== "admin") {
+        return {
+          message: "You are not authorized to create a day restriction",
+          type: "error",
+        };
+      }
+      const id = uuidv4();
+      const { error } = DayRestrictionSchema.validate({
+        scheduleRuleId,
+        dayOfWeek,
+        maxFollowingDay,
+      });
+      if (error) {
+        return {
+          message: error.details[0].message,
+          type: "error",
+        };
+      }
+      await DayRestrictionModal.create({
+        id,
+        scheduleRuleId,
+        dayOfWeek,
+        maxFollowingDay,
+      });
+      return {
+        type: "success",
+        message: "Day restriction created successfully",
+      };
+    } catch (error) {
+      return {
+        type: "error",
+        message: "An error occurred while creating day restriction: " + error,
+      };
+    }
   };
 }
 
