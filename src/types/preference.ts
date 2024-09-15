4f934bbcf13144329f7733c6faabf6660fffa745
podlaskialgorytm <live.m851@gmail.com>
2024-09-15 22:48:55 +0200

    feat: create schema for preference to validate it

---
+++ src/types/preference.ts
@@ -0,0 +1,11 @@
+import Joi from "Joi";
+
+export const PreferenceSchema = Joi.object({
+  shiftId: Joi.string().required(),
+  preferedDays: Joi.string().required(),
+  employmentTypeId: Joi.string().required(),
+}).messages({
+  "any.required": "{{#label}} is required",
+  "string.empty": "{{#label}} is required",
+  "string.base": "{{#label}} must be a string",
+});
