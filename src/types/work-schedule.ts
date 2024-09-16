f5be68fa8d4392070c0eb4186c7bb68199f59dae
podlaskialgorytm <live.m851@gmail.com>
2024-09-16 22:12:19 +0200

    feat: create schema to validate if work schedule data is correct

---
+++ src/types/work-schedule.ts
@@ -0,0 +1,17 @@
+import Joi from "Joi";
+
+export const WorkScheduleSchema = Joi.object({
+  groupUserId: Joi.string().required(),
+  year: Joi.number().required(),
+  month: Joi.number().required(),
+  day: Joi.string().required(),
+  isWorkingDay: Joi.boolean().required(),
+  start: Joi.string().required(),
+  end: Joi.string().required(),
+}).messages({
+  "any.required": "{{#label}} is required",
+  "string.empty": "{{#label}} is not allowed to be empty",
+  "number.base": "{{#label}} should be a number",
+  "boolean.base": "{{#label}} should be a boolean",
+  "string.base": "{{#label}} should be a string",
+});
