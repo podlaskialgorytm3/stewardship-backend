1eb1e3580bf0da211686c8445cd3d97dc826cb98
podlaskialgorytm <live.m851@gmail.com>
2024-09-16 17:00:05 +0200

    feat: create function to improve validation if given day in given month is exist

--- src/services/unavailable-day.ts
+++ src/services/unavailable-day.ts
@@ -3,6 +3,28 @@ import { UnavaiableDayModal } from "../models/unavailable-day";
 import { v4 as uuidv4 } from "uuid";
 
 class UnavailableDayService {
+  month: number;
+  year: number;
+  monthDays: Object;
+  constructor() {
+    this.month = new Date().getMonth() + 2 > 12 ? 1 : new Date().getMonth() + 2;
+    this.year =
+      this.month > 12 ? new Date().getFullYear() + 1 : new Date().getFullYear();
+    this.monthDays = {
+      1: 31,
+      2: this.year % 4 === 0 ? 29 : 28,
+      3: 31,
+      4: 30,
+      5: 31,
+      6: 30,
+      7: 31,
+      8: 31,
+      9: 30,
+      10: 31,
+      11: 30,
+      12: 31,
+    };
+  }
   public createTable = async () => {
     UnavaiableDayModal.sync({ alter: true })
       .then(() => {
@@ -29,6 +51,12 @@ class UnavailableDayService {
           message: "Please provide all required fields",
         };
       }
+      if (!(await this.checkIfUnavaiableDayExists({ day }))) {
+        return {
+          type: "error",
+          message: "Day does not exist",
+        };
+      }
       const id = uuidv4();
       await UnavaiableDayModal.create({
         id,
@@ -44,6 +72,16 @@ class UnavailableDayService {
         type: "error",
         message: "An error occurred while creating unavaiable day: " + error,
       };
+    }
+  };
+  private checkIfUnavaiableDayExists = async ({ day }: { day: string }) => {
+    try {
+      if (this.monthDays[this.month] < parseInt(day)) {
+        return false;
+      }
+      return true;
+    } catch (error) {
+      return false;
     }
   };
 }
