b1f1aff44f0f9511f24de523041dea972e8480de
podlaskialgorytm <live.m851@gmail.com>
2024-09-17 00:07:07 +0200

    feat:  service for creating work schedule in particular day

--- src/services/work-schedule.ts
+++ src/services/work-schedule.ts
@@ -1,17 +1,96 @@
 import { WorkScheduleModal } from "../models/work-schedule";
 
+import { WorkScheduleSchema } from "../types/work-schedule";
+
+import GroupUserService from "./group-user";
+
+import { v4 as uuidv4 } from "uuid";
+
 class WorkScheduleService {
+  groupUserService: GroupUserService;
+  constructor() {
+    this.groupUserService = new GroupUserService();
+  }
   public createTable = async () => {
     WorkScheduleModal.sync({ alter: true })
       .then(() => {
-        console.log("Skill table has been synchronized");
+        console.log("Working Schedule table has been synchronized");
       })
       .catch((error) => {
         console.error(
-          "An error occurred while synchronizing the Skill table:",
+          "An error occurred while synchronizing the Working Schedule table:",
           error
         );
       });
+  };
+  public createWorkSchedule = async ({
+    groupId,
+    groupUserId,
+    year,
+    month,
+    day,
+    isWorkingDay,
+    start,
+    end,
+    token,
+  }: {
+    groupId: string;
+    groupUserId: string;
+    year: number;
+    month: number;
+    day: number;
+    isWorkingDay: boolean;
+    start: string;
+    end: string;
+    token: string;
+  }) => {
+    try {
+      const role = (await this.groupUserService.getRole({
+        groupId,
+        token,
+      })) as string;
+      if (role !== "admin") {
+        return {
+          type: "error",
+          message: "You are not authorized to perform this action",
+        };
+      }
+      const { error } = WorkScheduleSchema.validate({
+        groupUserId,
+        year,
+        month,
+        day,
+        isWorkingDay,
+        start,
+        end,
+      });
+      if (error) {
+        return {
+          type: "error",
+          message: error.details[0].message,
+        };
+      }
+      const id = uuidv4();
+      await WorkScheduleModal.create({
+        id,
+        groupUserId,
+        year,
+        month,
+        day,
+        isWorkingDay,
+        start,
+        end,
+      });
+      return {
+        type: "success",
+        message: "Work Schedule created successfully",
+      };
+    } catch (error) {
+      return {
+        type: "error",
+        message: "An error occurred while creating the Work Schedule: " + error,
+      };
+    }
   };
 }
 
