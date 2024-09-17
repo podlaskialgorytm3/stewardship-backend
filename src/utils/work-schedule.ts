ee56582e3314a664fc9abcbbf262490a4062f828
podlaskialgorytm <live.m851@gmail.com>
2024-09-17 21:06:09 +0200

    feat: create class and util to calculations work time in given day

---
+++ src/utils/work-schedule.ts
@@ -0,0 +1,28 @@
+class WorkScheduleUtils {
+  public calculateWorkTimeDecimal({
+    startTime,
+    endTime,
+  }: {
+    startTime: string;
+    endTime: string;
+  }) {
+    const timeToMinutes = (time: string): number => {
+      const [hours, minutes] = time.split(":").map(Number);
+      return hours * 60 + minutes;
+    };
+
+    const startMinutes = timeToMinutes(startTime);
+    const endMinutes = timeToMinutes(endTime);
+
+    const totalMinutes =
+      endMinutes >= startMinutes
+        ? endMinutes - startMinutes
+        : 24 * 60 - startMinutes + endMinutes;
+
+    const totalHoursDecimal = totalMinutes / 60;
+
+    return totalHoursDecimal;
+  }
+}
+
+export { WorkScheduleUtils };
